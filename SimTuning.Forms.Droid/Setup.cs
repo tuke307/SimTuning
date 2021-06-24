747c5eca276acb59d8f41b16e90f65554c5538c3
Tony <tonymeissner70@gmail.com>
2021-06-24 09:29:15 +0200

    droid and ios changes, remove doubled nuget references

--- SimTuning.Forms.Droid/Setup.cs
+++ SimTuning.Forms.Droid/Setup.cs
@@ -1,15 +1,23 @@
 ﻿// project=SimTuning.Forms.Droid, file=Setup.cs, creation=2020:10:19 Copyright (c) 2021
 // tuke productions. All rights reserved.
+using Data;
+using Microsoft.Extensions.Logging;
 using MvvmCross;
 using MvvmCross.Forms.Platforms.Android.Core;
+using MvvmCross.IoC;
 using MvvmCross.Plugin;
 using MvvmCross.ViewModels;
+using Serilog;
+using Serilog.Extensions.Logging;
+using SimTuning.Core;
 using SimTuning.Forms.UI;
 using SimTuning.Forms.UI.Services;
+using System;
+using System.IO;
 
 namespace SimTuning.Forms.Droid
 {
-    public class Setup : MvxFormsAndroidSetup<MvxApp, FormsApp>
+    public class Setup : MvxFormsAndroidSetup<Forms.UI.MvxApp, FormsApp>
     // No Splash Screen with this; MvxAndroidSetup<App>
     {
         public override void LoadPlugins(IMvxPluginManager pluginManager)
@@ -21,14 +29,46 @@ namespace SimTuning.Forms.Droid
             pluginManager.EnsurePluginLoaded<MvvmCross.Plugin.Location.Fused.Plugin>();
         }
 
-        protected override IMvxApplication CreateApp()
+        protected override IMvxApplication CreateApp(IMvxIoCProvider iocProvider)
         {
-            return CreateApp();
+            return base.CreateApp(iocProvider);
         }
 
-        protected override void InitializeLastChance()
+        /// <inheritdoc />
+        protected override ILoggerFactory CreateLogFactory()
         {
-            base.InitializeLastChance();
+            // TODO: eigentlich nicht die richtige stelle-
+            // android: "/data/user/0/com.tuke_productions.SimTuning/files/"
+            SimTuning.Core.GeneralSettings.FileDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SimTuning"); // appdata-local-simtuning
+
+            if (string.IsNullOrEmpty(Data.DatabaseSettings.DatabasePath))
+            {
+                Data.DatabaseSettings.DatabasePath = Path.Combine(SimTuning.Core.GeneralSettings.FileDirectory, Data.DatabaseSettings.DatabaseName);
+            }
+
+            // appdata-local-simtuning ist anfangs nicht da
+            if (!Directory.Exists(SimTuning.Core.GeneralSettings.FileDirectory))
+            {
+                Directory.CreateDirectory(SimTuning.Core.GeneralSettings.FileDirectory);
+            }
+
+            // serilog configuration
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.File(GeneralSettings.LogFilePath)
+                .CreateLogger();
+
+            return new SerilogLoggerFactory();
+        }
+
+        protected override ILoggerProvider CreateLogProvider()
+        {
+            return new SerilogLoggerProvider();
+        }
+
+        protected override void InitializeFirstChance(IMvxIoCProvider iocProvider)
+        {
+            base.InitializeFirstChance(iocProvider);
             Mvx.IoCProvider.RegisterSingleton<IThemeService>(() => new ThemeService());
         }
     }
