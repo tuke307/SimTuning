4cef9ee8ab88003171d732939b4ad47cd010a92e
Tony Meissner <tonymeissner70@gmail.com>
2023-01-20 15:59:29 +0100

    Maui App hinzugefügt mit plattform targets

---
+++ src/SimTuning.Maui.App/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using SimTuning.Core.Services;
+using SimTuning.Core.ViewModels;
+
+namespace SimTuning.Maui.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            LiveCharts.Configure(config =>
+               config
+                   // registers SkiaSharp as the library backend
+                   // REQUIRED unless you build your own
+                   .AddSkiaSharp()
+
+                   // adds the default supported types
+                   // OPTIONAL but highly recommend
+                   .AddDefaultMappers()
+               // .HasMap<Foo>( .... )
+               // .HasMap<Bar>( .... )
+               );
+
+            //MainPage = new NavigationPage();
+            //navigationService.Navigate<MainPageViewModel>();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/SimTuning.Maui.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SimTuning.Maui.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
