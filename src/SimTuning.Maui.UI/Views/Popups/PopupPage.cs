b4237eb9e64aeb3010dd309b89b87183dad27e28
Tony Meissner <tonymeissner70@gmail.com>
2023-02-17 23:36:02 +0100

    popup page

---
+++ src/SimTuning.Maui.UI/Views/Popups/PopupPage.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace SimTuning.Maui.UI.Views.Popups
+{
+    public partial class PopupPage : Popup
+    {
+        public PopupPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
